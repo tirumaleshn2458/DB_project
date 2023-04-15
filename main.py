b036df16366e04114a1108a5445acf072d0b006b
tirumaleshn2458 <tirumaleshn2000@gmail.com>
2023-04-15 03:00:15 -0500

    Updated the table creation and application

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import streamlit as st
 import time
+import psycopg2
+import random
+import string
+
+
 st.title('Real Estate Agency')
+
+
+def query_execution(query,variables):
+    connection=psycopg2.connect(database='real_estate_db',user='tirumaleshn2000',password='',host='localhost',port='5431')
+    cursor=connection.cursor()
+    cursor.execute(query,variables)
+    record=cursor.fetchall()
+    connection.commit()
+    connection.close()
+    return record
+
+def entry_execution(query,variables):
+    connection=psycopg2.connect(database='real_estate_db',user='tirumaleshn2000',password='',host='localhost',port='5431')
+    cursor=connection.cursor()
+    cursor.execute(query,variables)
+    connection.commit()
+    connection.close()
+
+
+
 
 #user status
 user_status=None
@@ -59,23 +84,106 @@ ob_pro=proceed()
 
 ob_login=login()
 
+sign_or_login_empty=st.empty()
 user_type_empty=st.empty()
-username_empty=st.empty()
+email_id_empty=st.empty()
 password_empty=st.empty()
 login_button_empty=st.empty()
-if len(ob_login.user_status_list)==0:
+
+
+def get_random_string(length):
+    letters = string.ascii_lowercase+string.digits
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+def login_page():
     user_type=user_type_empty.radio('',options=['Agent','Renter'])
-    username=username_empty.text_input('Username: ')
+    email_id=email_id_empty.text_input('Email: ')
     password=password_empty.text_input('Password: ',type='password')
     if login_button_empty.button('Login'):
-        ob_login.user_status_list.append('proceed')
-        ob_login.user_status_list.append(username)
-        ob_login.user_status_list.append(password)
-        ob_login.user_status_list.append(user_type)
-        username_empty.empty()
-        password_empty.empty()
-        login_button_empty.empty()
-        user_type_empty.empty()
+        if len(email_id)!=0:
+            if len(password)!=0:
+                login_query='select * from users where email_id = %s and password = %s and user_type = %s'
+                login_variables=(email_id,password,user_type)
+                records=query_execution(login_query,login_variables)
+                if len(records)!=0:
+                    ob_login.user_status_list.append('proceed')
+                    ob_login.user_status_list.append(email_id)
+                    ob_login.user_status_list.append(password)
+                    ob_login.user_status_list.append(user_type)
+                    email_id_empty.empty()
+                    password_empty.empty()
+                    login_button_empty.empty()
+                    user_type_empty.empty()
+                else:
+                    st.error('Invalid login details')
+            else:
+                st.error('Please enter the password')
+        else:
+            st.error('Please enter the username')
+
+def register():
+    st.write('Please fill the details to register')
+    fn_empty=st.empty()
+    mn_empty=st.empty()
+    ln_empty=st.empty()
+    email_empty=st.empty()
+    ut_empty=st.empty()
+    agency_empty=st.empty()
+    contact_empty=st.empty()
+    ad_empty=st.empty()
+    pass_empty=st.empty()
+    repass_empty=st.empty()
+    register_button_empty=st.empty()
+    first_name=fn_empty.text_input('First Name')
+    middle_name=mn_empty.text_input('Middle Name')
+    last_name=ln_empty.text_input('Last Name')
+    email_id=email_empty.text_input('Email ID')
+    user_type=ut_empty.selectbox('User Type',options=['Agent','Renter'])
+    if user_type=='Agent':
+        real_estate_agency=
+    address=ad_empty.text_input('Address')
+    password=pass_empty.text_input('Enter password')
+    repassword=repass_empty.text_input('Re Enter the password')
+    if register_button_empty.button('Register'):
+        while True:
+            user_id=get_random_string(7)
+            query='select * from users where user_id = %s'
+            values=(user_id,)
+            result=query_execution(query,values)
+            if len(result)==0:
+                register_query='''insert into users (first_name,middle_name,last_name,user_type,address,email_id,password,user_id)
+                values (%s,%s,%s,%s,%s,%s,%s,%s)'''
+                values=(first_name,middle_name,last_name,user_type,address,email_id,password,user_id)
+                entry_execution(register_query,values)
+                st.success('Registered successfully')
+                fn_empty.empty()
+                mn_empty.empty()
+                ln_empty.empty()
+                email_empty.empty()
+                ut_empty.empty()
+                ad_empty.empty()
+                pass_empty.empty()
+                repass_empty.empty()
+                register_button_empty.empty()
+                break
+            else:
+                pass
+
+
+
+
+
+if len(ob_login.user_status_list)==0:
+    sign_or_login=sign_or_login_empty.radio('',options=['Login','Register'])
+    if sign_or_login=='Login':
+        login_page()
+    elif sign_or_login=='Register':
+        register()
+
+
+
 
 
 if 'proceed' in ob_login.user_status_list:
--- table_creation.py
+++ table_creation.py
@@ -1,21 +1,5 @@
 import psycopg2
-#creating the property table
-property_table='''create table if not exists property(
-property_id varchar(8) not null primary key,
-type varchar(20) not null,
-description varchar(250) not null,
-availability varchar(10) not null,
-price float not null
-);'''
 
-property_address='''create table if not exists property_address(
-property_id varchar(8) not null,
-city varchar(30) not null,
-state varchar(20) not null,
-location varchar(50) not null,
-primary key(property_id,city,state,location),
-foreign key(property_id) references property
-);'''
 
 #creating the user table
 users='''create table if not exists users(
@@ -25,7 +9,8 @@ middle_name varchar(30) not null,
 last_name varchar(30) not null,
 user_type varchar(20) not null,
 address varchar(50) not null,
-email_id varchar(40) not null
+email_id varchar(40) not null,
+password varchar(30) not null
 );'''
 
 #creating the renter table
@@ -38,13 +23,45 @@ budget float,
 foreign key(user_id) references users
 );'''
 
+
+#agents
+agents='''create table if not exists agents(
+agents_id varchar(8) not null primary key,
+user_id varchar(8) not null,
+realestate_agency varchar(20) not null,
+contact varchar(15) not null,
+job_title varchar(20) not null
+);'''
+
+#agents_contact
+agents_contact='''create table if not exists agents_contact(
+agents_id varchar(8) not null,
+contact varchar(8) not null,
+primary key(agents_id,contact),
+foreign key(agents_id) references agents)'''
+
+#creating the property table
+property_table='''create table if not exists property(
+property_id varchar(8) not null primary key,
+agents_id varchar(8) not null,
+type varchar(20) not null,
+city varchar(30) not null,
+state varchar(20) not null,
+location varchar(50) not null,
+description varchar(250) not null,
+availability varchar(10) not null,
+price float not null,
+foreign key (agents_id) references agents
+);'''
+
 #creditcard
 credit_card='''create table if not exists credit_card(
 creditcard_id varchar(8) primary key,
 card_number varchar(30),
 renter_id varchar(8),
 address varchar(50),
-card_expiry_date varchar(8)
+card_expiry_date varchar(8),
+foreign key renter_id references(renters),
 );'''
 
 #property_booking
@@ -54,23 +71,10 @@ property_id varchar(8) not null,
 renters_id varchar(8) not null,
 booking_date date not null,
 creditcard_id varchar(8) not null,
-primary key(booking_id,property_id,renters_id),
+primary key(booking_id,property_id,renters_id,creditcard_id),
 foreign key(property_id) references property,
 foreign key(renters_id) references renter,
 foreign key(creditcard_id) references credit_card);'''
-
-#agents
-agents='''create table if not exists agents(
-agents_id varchar(8) not null primary key,
-user_id varchar(8) not null,
-realestate_agency varchar(20) not null,
-contact varchar(15) not null,
-job_title varchar(20) not null,
-booking_id varchar(8) not null,
-property_id varchar(8) not null,
-renters_id varchar(8) not null,
-foreign key(booking_id,renters_id,property_id) references property_booking(booking_id,renters_id,property_id) on delete cascade
-);'''
 
 #house
 house='''create table if not exists house(
@@ -106,10 +110,9 @@ booking_id varchar(8) not null,
 property_id varchar(8) not null,
 renters_id varchar(8) not null,
 creditcard_id varchar(8) not null,
-primary key(booking_id,property_id,renters_id),
-foreign key(booking_id,property_id,renters_id) references property_booking(booking_id,property_id,renters_id)
+primary key(booking_id,property_id,renters_id,creditcard_id),
+foreign key(booking_id,property_id,renters_id,creditcard_id) references property_booking(booking_id,property_id,renters_id,creditcard_id)
 );'''
-
 
 
 #user_renter
@@ -162,11 +165,13 @@ primary key(building_id, property_id),
 foreign key(property_id) references property,
 foreign key(building_id) references commercial_building );'''
 
+
 connection=psycopg2.connect(database='real_estate_db',user='tirumaleshn2000',password='',host='localhost',port='5431')
 cursor=connection.cursor()
-for table in [property_table,property_address,users,renter,credit_card,
-              property_booking,agents,house,apartment,commercial_building,property_credit,
-             user_renter,user_agent,searches,property_house,property_apts,property_build]:
+for table in [users,renter,agents,agents_contact,property_table,
+              credit_card,property_booking,house,apartment,commercial_building,
+              property_credit,user_renter,user_agent,searches,
+              property_house,property_apts,property_build]:
     print('----------')
     print(table)
     cursor.execute(table)
